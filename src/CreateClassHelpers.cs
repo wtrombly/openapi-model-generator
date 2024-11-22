6c4dcc539e53b3c33a1d5477baaad0716cb75008
wtrombly <76636215+wtrombly@users.noreply.github.com>
2024-11-22 13:35:29 -0600

    initial commit

---
+++ src/CreateClassHelpers.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.OpenApi.Models;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace OpenAPIModelGenerator.Models;
+
+internal class CreateClassHelpers
+{
+    public static ClassDeclarationSyntax CreateClass(string name, OpenApiSchema classType) =>
+    ClassDeclaration(Identifier(name))
+        .AddModifiers(Token(SyntaxKind.PublicKeyword))
+        .AddMembers(classType.Properties.Select(property =>
+            PropertyDeclaration(
+                GetDataType(property.Value), Identifier(ToPascalCase(property.Key)))
+            .AddModifiers(Token(SyntaxKind.PublicKeyword))
+            .AddAccessorListAccessors(
+                AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
+                    .WithSemicolonToken(Token(SyntaxKind.SemicolonToken)),
+                AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
+                    .WithSemicolonToken(Token(SyntaxKind.SemicolonToken))
+            )
+        .WithAttributeLists(
+            SingletonList<AttributeListSyntax>(
+                AttributeList(
+                    SingletonSeparatedList<AttributeSyntax>(
+                        Attribute(
+                            IdentifierName("JsonProperty"))
+                        .WithArgumentList(
+                            AttributeArgumentList(
+                                SingletonSeparatedList<AttributeArgumentSyntax>(
+                                    AttributeArgument(
+                                        LiteralExpression(
+                                            SyntaxKind.StringLiteralExpression,
+                                            Literal(name))))))))))
+        ).ToArray());
+
+    private static string ToPascalCase(string input)
+    {
+        input = Regex.Replace(input, @"[^a-zA-Z0-9\s]", "");
+
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        string titleCase = textInfo.ToTitleCase(input.ToLower());
+
+        return titleCase.Replace(" ", "");
+    }
+
+    private static TypeSyntax GetDataType(OpenApiSchema propertyType)
+    {
+        if (propertyType.Type.Contains("int"))
+        {
+            return propertyType.Nullable ? NullableType(PredefinedType(Token(SyntaxKind.IntKeyword))) : PredefinedType(Token(SyntaxKind.IntKeyword));
+        }
+        if (propertyType.Type.Contains("number"))
+        {
+            return propertyType.Nullable ? NullableType(PredefinedType(Token(CheckNumberFormat(propertyType.Format)))) : PredefinedType(Token(CheckNumberFormat(propertyType.Format)));
+        }
+        if (propertyType.Type.Contains("bool"))
+        {
+            return propertyType.Nullable ? NullableType(PredefinedType(Token(SyntaxKind.BoolKeyword))) : PredefinedType(Token(SyntaxKind.BoolKeyword));
+        }
+        if (string.Equals(propertyType.Type, "object"))
+        {
+            if (propertyType.Reference is not null)
+            {
+                return propertyType.Nullable ? NullableType(IdentifierName(propertyType.Reference.Id)) : IdentifierName(propertyType.Reference.Id);
+            }
+            else
+            {
+                return IdentifierName("object");
+            }
+        }
+        if (string.Equals(propertyType.Type, "array"))
+        {
+            if (propertyType.Items.Reference is null || propertyType.Items.Type is null)
+            {
+                return propertyType.Nullable ?
+                                    NullableType(ArrayType(IdentifierName("object"))
+                                    .WithRankSpecifiers(
+                                    SingletonList(
+                                        ArrayRankSpecifier(
+                                            SingletonSeparatedList<ExpressionSyntax>(
+                                                OmittedArraySizeExpression()))))) :
+                                    ArrayType(IdentifierName("object"))
+                                    .WithRankSpecifiers(
+                                    SingletonList(
+                                        ArrayRankSpecifier(
+                                            SingletonSeparatedList<ExpressionSyntax>(
+                                                OmittedArraySizeExpression()))));
+            }
+            if (string.Equals(propertyType.Items.Type, "object"))
+            {
+                return propertyType.Nullable ?
+                    NullableType(ArrayType(IdentifierName(propertyType.Items.Reference.Id))
+                    .WithRankSpecifiers(
+                    SingletonList(
+                        ArrayRankSpecifier(
+                            SingletonSeparatedList<ExpressionSyntax>(
+                                OmittedArraySizeExpression()))))) :
+                    ArrayType(IdentifierName(propertyType.Items.Reference.Id))
+                    .WithRankSpecifiers(
+                    SingletonList(
+                        ArrayRankSpecifier(
+                            SingletonSeparatedList<ExpressionSyntax>(
+                                OmittedArraySizeExpression()))));
+            }
+            return propertyType.Nullable ? NullableType(ArrayType(IdentifierName(propertyType.Items.Type))) : ArrayType(IdentifierName(propertyType.Items.Type));
+        }
+        return propertyType.Nullable ? NullableType(NullableType(PredefinedType(Token(SyntaxKind.StringKeyword)))) : PredefinedType(Token(SyntaxKind.StringKeyword));
+    }
+
+    private static SyntaxKind CheckNumberFormat(string format)
+    {
+        if (string.Equals(format, "float"))
+        {
+            return SyntaxKind.FloatKeyword;
+        }
+        if (string.Equals(format, "double"))
+        {
+            return SyntaxKind.DoubleKeyword;
+        }
+        if (string.Equals(format, "decimal"))
+        {
+            return SyntaxKind.DecimalKeyword;
+        }
+        if (string.Equals(format, "long"))
+        {
+            return SyntaxKind.LongKeyword;
+        }
+        return SyntaxKind.DoubleKeyword;
+    }
+}
---
+++ src/ModelGenerator.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Readers;
+using OpenAPIModelGenerator.Models;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace OpenAPIModelGenerator
+{
+    public class ModelGenerator
+    {
+        private readonly string _inputFilePath;
+        private readonly string _outputFilePath;
+        private readonly string _outPutNameSpace;
+        private readonly ILogger _logger;
+
+        public ModelGenerator(ILogger<ModelGenerator> logger, string inputFilePath, string outputFilePath, string outPutNameSpace = "CodeGen")
+        {
+            if (string.IsNullOrWhiteSpace(inputFilePath))
+                throw new ArgumentException("Input file path cannot be null or empty.", nameof(inputFilePath));
+            if (string.IsNullOrWhiteSpace(outputFilePath))
+                throw new ArgumentException("Output file path cannot be null or empty.", nameof(outputFilePath));
+
+            _inputFilePath = inputFilePath;
+            _outputFilePath = outputFilePath;
+            _outPutNameSpace = outPutNameSpace;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Method to execute the entire workflow
+        /// </summary>
+        /// <returns></returns>
+        public async Task Execute()
+        {
+            MemberDeclarationSyntax[] members;
+            OpenApiDocument document;
+            try
+            {
+                document = await ReadInputFileAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to read openApiDocument", ex);
+            }
+
+            try
+            {
+                members = Compute(document);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to create classes", ex);
+            }
+
+            try
+            {
+                await WriteOutputFile(members);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to write to files", ex);
+            }
+        }
+
+        /// <summary>
+        /// Reads and the input file and logs information.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        private async Task<OpenApiDocument> ReadInputFileAsync()
+        {
+            if (!File.Exists(_inputFilePath))
+                throw new FileNotFoundException($"Input file not found at {_inputFilePath}.");
+
+            OpenApiDocument document;
+            try
+            {
+                await using var fileStream = File.OpenRead(_inputFilePath);
+                document = new OpenApiStreamReader().Read(fileStream, out var diagnostics);
+
+                _logger.LogInformation("Document read with the following Spec Version: {SpecVersion}",
+                    diagnostics.SpecificationVersion.ToString());
+
+                _logger.LogWarning("Warnings Count: {WarningsCount}. This may disrupt code generation.",
+                    diagnostics.Warnings.Count);
+
+                _logger.LogError("Errors Count: {ErrorsCount}. This may disrupt code generation.",
+                    diagnostics.Errors.Count);
+
+
+                if (diagnostics != null)
+                {
+                    foreach (var diagnosticItem in diagnostics.Warnings)
+                    {
+                        _logger.LogWarning("Open API Warning: {diagnosticItem.Message}", diagnosticItem.Message);
+                    }
+                    foreach (var diagnosticItem in diagnostics.Errors)
+                    {
+                        _logger.LogWarning("Open API Error: {diagnosticItem.Message}", diagnosticItem.Message);
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("Failed to read input file.", ex);
+            }
+
+            return document;
+        }
+
+        /// <summary>
+        /// Performs computations or logic on the input data
+        /// </summary>
+        /// <param name="openApiDocument"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        private static MemberDeclarationSyntax[] Compute(OpenApiDocument openApiDocument)
+        {
+            return openApiDocument.Components.Schemas?.Select(t => CreateClassHelpers.CreateClass(t.Key, t.Value)).ToArray() ?? Array.Empty<MemberDeclarationSyntax>();
+        }
+
+        /// <summary>
+        /// Writes the computed data to the output file
+        /// </summary>
+        /// <param name="computedData"></param>
+        private async Task WriteOutputFile(MemberDeclarationSyntax[] members)
+        {
+
+            foreach (var member in members)
+            {
+                if (member is ClassDeclarationSyntax classDeclaration)
+                {
+                    // Create the namespace and add the class
+                    var ns = NamespaceDeclaration(ParseName(_outPutNameSpace))
+                        .AddMembers(member);
+
+                    // Generate the using directive
+                    var usingDirective = UsingDirective(
+                        QualifiedName(
+                            IdentifierName("Newtonsoft"),
+                            IdentifierName("Json")));
+
+                    // Combine the using directive and namespace into a single compilation unit
+                    var compilationUnit = CompilationUnit()
+                        .AddUsings(usingDirective)
+                        .AddMembers(ns)
+                        .NormalizeWhitespace();
+
+                    // Write the result to the file
+                    Directory.CreateDirectory(_outputFilePath);
+                    await using var streamWriter = new StreamWriter(_outputFilePath, false);
+                    compilationUnit.WriteTo(streamWriter);
+                }
+            }
+        }
+    }
+}
